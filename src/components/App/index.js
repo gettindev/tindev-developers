02d4922a348876a9696c183c1fb9890090e65610
Developer-Musclay <jeremydumascontact@gmail.com>
2019-10-17 14:14:32 +0200

    homePage component ready

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,12 +7,14 @@ import './app.scss';
 // == Import : style
 
 // Import locaux
-import Nav from 'src/components/Nav';
+// import Nav from 'src/components/Nav';
+import HomePage from 'src/components/HomePage'
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Nav />
+    {/* <Nav /> */}
+    <HomePage />
   </div>
 );
 
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,16 @@
+// == Import : npm
+import React from 'react';
+
+// imports styles
+// import { FaUserAlt, FaRegCommentDots } from "react-icons/fa";
+
+// == Import : local
+import './homePage.scss';
+
+// == Composant
+const HomePage = () => (
+  <div>Tindev</div>
+);
+
+// == Export
+export default HomePage;
