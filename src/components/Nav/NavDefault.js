08f818ab081c1a7f8c5ec4ae96caebcd442c95ae
Damien Tailhades <damien.tailhades@gmail.com>
2019-10-21 10:32:44 +0200

    ADD: tindev logotype with white background on the Nav component

--- src/components/Nav/NavDefault.js
+++ src/components/Nav/NavDefault.js
@@ -3,13 +3,14 @@ import React from 'react';
 
 // imports local
 import './nav.scss';
+import svgLogo from 'src/data/logo-tindev-white-background.svg';
 import { MdAccountCircle, MdChat } from 'react-icons/md';
 import { Navbar } from 'react-bootstrap';
 
 const NavDefault = () => (
   <Navbar expand={false} className="td-navbar">
     <Navbar.Text className="icon" onClick={(e) => {console.log('>>> User Button', e)}}><MdAccountCircle /></Navbar.Text>
-    <Navbar.Text className="td-navbar-logo">Tindev</Navbar.Text>
+    <Navbar.Text className="td-navbar-logo"><img src={svgLogo} alt="Tindev" height="33"/></Navbar.Text>
     <Navbar.Text className="icon" onClick={(e) => {console.log('>>> User Chat', e)}}><MdChat /></Navbar.Text>
   </Navbar>
 );
