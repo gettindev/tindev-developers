8c8d68a1e5b7fc7cba34002bfa64bbb89eac13f2
Damien Tailhades <damien.tailhades@gmail.com>
2019-10-21 17:52:11 +0200

    DONE: user profil edit page

--- src/components/UserProfil/EditUserProfil.js
+++ src/components/UserProfil/EditUserProfil.js
@@ -1,9 +1,15 @@
 // == Import : npm
 import React from 'react';
+// this one generate warnings in the console because using componentWillReceiveProps()
+import { Typeahead } from 'react-bootstrap-typeahead';
 // import PropTypes from 'prop-types';
 
 // == Import : local
 import './user-profil.scss';
+import './edit-profil.scss';
+// css for the Typeahead
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+//
 import Nav from 'src/components/Nav';
 import {
   Container,
@@ -14,6 +20,7 @@ import {
   InputGroup,
   FormControl,
 } from 'react-bootstrap';
+// data for the tech select
 
 // == Composant
 class EditUserProfil extends React.Component {
@@ -24,11 +31,13 @@ class EditUserProfil extends React.Component {
     biography: "I'm a designer trying to become developer after 12 years designing website and mobile apps",
     level: '1',
     technos: ['html', 'php', 'javascript'],
+    sharedNewUrl: '',
+    sharedNewTitleUrl: '',
     sharedUrl: [
-      { id: 0, name: 'Google', url: 'http://www.google.com' },
-      { id: 1, name: 'Regulardesigner', url: 'http://www.regulardesigner.com' },
-      { id: 2, name: 'Webibli', url: 'http://www.webibli.fr' }
-    ]
+      { id: 0, title: 'Google', url: 'http://www.google.com' },
+      { id: 1, title: 'Regular', url: 'http://www.regulardesigner.com' },
+      { id: 2, title: 'Webibli', url: 'http://www.webibli.fr' },
+    ],
   }
 
   handleChange = (event) => {
@@ -38,9 +47,58 @@ class EditUserProfil extends React.Component {
     });
   }
 
+  removeInArrayFromId = (identifier) => (event) => {
+    event.preventDefault();
+    // code here...
+    const { sharedUrl } = this.state;
+    const remodedSharedUrl = sharedUrl.filter(url => url.id !== identifier);
+    this.setState({
+      sharedUrl: remodedSharedUrl,
+    });
+  }
+
+  generateId = () => {
+    const { sharedUrl } = this.state;
+    const ids = sharedUrl.map((url) => url.id);
+    const maxId = ids.length === 0 ? 0 : Math.max(...ids);
+    const newId = maxId + 1;
+    return newId;
+  }
+
+  handleAddUrl = () => {
+    // console.log(this.state.sharedNewUrl);
+    let { sharedNewUrl } = this.state;
+    let { sharedNewTitleUrl } = this.state;
+    const { sharedUrl } = this.state;
+    const urlId = this.generateId();
+    const updateSharedUrl = sharedUrl;
+
+    if (sharedUrl && sharedNewTitleUrl) {
+      updateSharedUrl.unshift({
+        id: urlId, title: sharedNewTitleUrl, url: sharedNewUrl,
+      });
+      this.setState({
+        sharedUrl: updateSharedUrl,
+        sharedNewUrl: '',
+        sharedNewTitleUrl: '',
+      });
+    }
+
+  }
+
   render() {
     const userName = 'myGithubName';
-    const { avatar, firstName, lastName, biography, level, technos, sharedUrl } = this.state;
+    const {
+      avatar,
+      firstName,
+      lastName,
+      biography,
+      level,
+      technos,
+      sharedNewUrl,
+      sharedNewTitleUrl,
+      sharedUrl,
+    } = this.state;
     return (
       <div className="edit-profil">
         <Nav nav="close-right" title="Éditer mon profil" />
@@ -63,7 +121,14 @@ class EditUserProfil extends React.Component {
             </Form.Group>
             <Form.Group controlId="formBiography">
               <Form.Label>Bio</Form.Label>
-              <Form.Control as="textarea" rows="4" placeholder="Petite description qui parle de toi..." name="biography" value={biography} onChange={this.handleChange} />
+              <Form.Control
+                as="textarea"
+                rows="4"
+                placeholder="Petite description qui parle de toi..."
+                name="biography"
+                value={biography}
+                onChange={this.handleChange}
+              />
               <Form.Text className="text-muted">
                 Ici tu peux parler de toi, de tes side-projects et bien d'autre chose encore !
               </Form.Text>
@@ -79,39 +144,50 @@ class EditUserProfil extends React.Component {
                 <option value="4">Binary God (6 ans et plus)</option>
               </Form.Control>
             </Form.Group>
-            <Form.Group controlId="formTechnos">
+            <Form.Group>
               <Form.Label>Technos utilisées</Form.Label>
-              <InputGroup>
-                <FormControl placeholder="Ajoutes des technos" />
-                <InputGroup.Append>
-                  <Button variant="primary">Ajouter</Button>
-                </InputGroup.Append>
-              </InputGroup>
-              <Form.Text className="text-muted">
-                {technos}
-              </Form.Text>
+              <Typeahead
+                className="technosChoiceField"
+                multiple
+                onChange={(technos) => { this.setState({ technos }); }}
+                selected={technos}
+                options={['php', 'javascript', 'JSX', 'Ruby', 'Python', 'MongoBD']}
+                name="technos"
+                id="formTechnos"
+              />
             </Form.Group>
 
             <Form.Group controlId="formSharedUrls">
               <Form.Label>Si tu veux montrer une url sur ton profil</Form.Label>
+              <FormControl className="mb-3" placeholder="Donnes un titre à ton url" value={sharedNewTitleUrl} name="sharedNewTitleUrl" onChange={this.handleChange} />
               <InputGroup className="mb-3">
-                <FormControl
-                  placeholder="http://whatever.com/"
-                />
+                <FormControl placeholder="http://whatever.com/" value={sharedNewUrl} name="sharedNewUrl" onChange={this.handleChange} />
                 <InputGroup.Append>
-                  <Button variant="primary">Ajouter</Button>
+                  <Button variant="primary" onClick={this.handleAddUrl}>Ajouter</Button>
                 </InputGroup.Append>
               </InputGroup>
-              <Form.Text className="text-muted">
-                Ajouter des urls à votre profil.
-              </Form.Text>
-              <ul>
-                {sharedUrl.map((url) => <li key={url.id}><a href={url.url}>{url.name}</a> <span className="text-danger text-sm">supprimer</span></li>)}
+              <Form.Text className="text-muted">Ajouter des urls à votre profil</Form.Text>
+              <ul className="edit-profil-url-list">
+                {sharedUrl.map((url) => {
+                  return (
+                    <li key={url.id}>
+                      <a className="link" href={url.url}>{url.title} <span className="edit-profil-url-list-url">({url.url})</span></a>
+                      <a className="text-danger text-sm suppr" href="#delete" onClick={this.removeInArrayFromId(url.id)}>Supprimer</a>
+                    </li>
+                  );
+                })}
               </ul>
             </Form.Group>
 
-            <Button onClick={(e) => {e.preventDefault(); console.log(this.state)}} block variant="primary" type="submit">Sauvegarder</Button>
+            <Button className="mb-10" onClick={(e) => { e.preventDefault(); console.log(this.state) }} block variant="primary" type="submit">Sauvegarder</Button>
           </Form>
+          <Row>
+            <Col className="mt-5">
+              <h4 className="text-danger">Wow, zone dangereuse !</h4>
+              <Button type="button" block variant="outline-danger">Supprimer mon compte</Button>
+              <small className="text-muted">Toutes les informations de ton compte seront définitivement supprimées. Nous ne conservons aucune données.</small>
+            </Col>
+          </Row>
         </Container>
       </div>
     );
