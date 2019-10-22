2f4392feb92f0c222fd0ba8a41bf8d002350d230
Damien Tailhades <damien.tailhades@gmail.com>
2019-10-22 11:28:19 +0200

    CHANGE: show user informations by getting them from a global state (REACT-REDUX)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,13 +9,15 @@ import './app.scss';
 // Import locaux
 import UserMenu from 'src/components/User/Menu';
 import EditUserProfil from 'src/components/User/Profil/Edit';
-import ShowUserProfil from 'src/components/User/Profil/Show';
+import ShowUserProfil from 'src/containers/User/Profil/Show';
 // import EditUserProfil from 'src/components/UserProfil/EditUserProfil';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <EditUserProfil />
+    {/* <UserMenu /> */}
+    {/* <EditUserProfil /> */}
+    <ShowUserProfil />
   </div>
 );
 
--- src/components/User/Profil/Show/index.js
+++ src/components/User/Profil/Show/index.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // import './show-profil.scss';
 
-const ShowUserProfil = () => (
-  <div className="show-profil">
-    <h1>This is the user profil show page.</h1>
-  </div>
-);
+const ShowUserProfil = ({ user }) => {
+  const userlinks = user.links;
+
+  return (
+    <div className="show-profil">
+      <img src={user.avatar} alt={`Avatar de ${user.firstName} ${user.lastName}`} />
+      <h2>{`${user.firstName} ${user.lastName}`}</h2>
+      <h3>Level ({user.level}) <small>{user.githubName}</small></h3>
+      <section className="bio">
+        <p>{user.biography}</p>
+      </section>
+      <section className="wishes">
+        <h4>Mes attentes :</h4>
+        {user.wishes}
+      </section>
+      <section className="technos">
+        <h4>Mes technos :</h4>
+        {user.technos}
+      </section>
+      <section className="links">
+        <ul>
+          {userlinks.map((link) => (
+            <li><a href={link.url}>{link.title}</a></li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+ShowUserProfil.propTypes = {
+  user: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    level: PropTypes.number.isRequired,
+    githubName: PropTypes.string.isRequired,
+    biography: PropTypes.string.isRequired,
+    wishes: PropTypes.array.isRequired,
+    technos: PropTypes.array.isRequired,
+    links: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+  }).isRequired,
+};
 
 export default ShowUserProfil;
