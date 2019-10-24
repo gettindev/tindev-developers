ddf089434480d5f9f3ee1c9c94bddb7f74a5d5b2
FlorianMrn <florian.merrien@hotmail.fr>
2019-10-24 09:18:02 +0200

    Nav and Route added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,6 +15,7 @@ import MatchingContainer from 'src/containers/MatchingContainer';
 import UserMenu from 'src/components/User/Menu';
 import EditUserProfil from 'src/components/User/Profil/Edit';
 import ShowUserProfil from 'src/containers/User/Profil/Show';
+import Nav from 'src/components/Nav';
 
 // == Composant
 const App = () => (
@@ -22,6 +23,7 @@ const App = () => (
     {/*<Page logged={true} />*/}
     {/* <Messages /> */}
     {/* <Form /> */}
+    <Nav nav={logo} />
     <MatchingContainer />
     {/* <UserMenu /> */}
     {/* <EditUserProfil /> */}
