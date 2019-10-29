8718a23f2a2df83544be64922f5542b204b19ae6
Damien Tailhades <damien.tailhades@gmail.com>
2019-10-29 15:49:14 +0100

    CREATE: static messages data

---
+++ server/static/messages.js
@@ -0,0 +1,28 @@
+const messages = [
+  {
+    id: 1,
+    content: 'Hello, comment vas tu ?',
+    sender: 1,
+    receiver: 2,
+  },
+  {
+    id: 2,
+    content: 'Niquel et toi ? Tu bosses sur quoi quel projet déjà ?',
+    sender: 2,
+    receiver: 1,
+  },
+  {
+    id: 3,
+    content: 'Coucou ! j\'ai un super projet open source pour toi !',
+    sender: 4,
+    receiver: 2,
+  },
+  {
+    id: 4,
+    content: 'Hi! New on the platform?',
+    sender: 5,
+    receiver: 10,
+  },
+];
+
+module.exports = messages;
